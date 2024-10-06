f37c795f34733875f9c12e277cc4c51c68a9149c
Vinícius Morais Silva <vmsabc@gmail.com>
2024-10-05 21:03:45 -0300

    Basic log done

--- src/server/middleware.ts
+++ src/server/middleware.ts
@@ -1,19 +1,37 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, response, Response } from "express";
 import { BaseError } from "../modules/error/model/model";
+import { container } from "tsyringe";
+import { WriteLog } from "../modules/logger/service/WriteLog";
 
 export const errorHandlerMiddleware = async (err: Error, req: Request, res: Response, _next: NextFunction): Promise<express.Response<any, Record<string, any>>> => {
+    
+    const writeErrorLog = container.resolve(WriteLog);
+    await writeErrorLog.execute({
+        level: 'error',
+        request: JSON.stringify({body: req.body, route: req.route}),
+        response: '',
+        error: JSON.stringify(err)
+    });
+    
     if (err instanceof BaseError) {
-        // Future Log
-        return res.status(err.statusCode)
-        .json({message: err.messageError});
+        return res.status(err.statusCode).json({message: err.messageError});
     }
 
     return res.status(500).json({ message: 'Internal server error'});
 }
 
 export const successHandlerMiddleware = async (req: Request, res: Response, _next: NextFunction): Promise<express.Response<any, Record<string, any>>> => {
+
+    const writeSuccessLog = container.resolve(WriteLog);
+    await writeSuccessLog.execute({
+        level: 'info',
+        request: JSON.stringify({body: req.body, route: req.route}),
+        response: JSON.stringify(response),
+        error: ''
+    });
+
     res.json(res.locals.data).status(res.locals.status);
-    // Future Log
+
     return res;
 }
 
