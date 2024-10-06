a756ca629bbc5465b196b27ea330f6ff3123e370
Vinícius Morais Silva <vmsabc@gmail.com>
2024-10-06 03:23:18 -0300

    Fixing connection

--- src/shared/database/connection.ts
+++ src/shared/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 import { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_USER } from "../utils/settings";
 
-export const database = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB}`,
-    { dialectModule: require('pg') }
+export const database = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@172.21.0.1:5432/${POSTGRES_DB}`
+    , { dialectModule: require('pg') }
 );
